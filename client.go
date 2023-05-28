2125cd2753cf308a20a81f47e52b5c4a48e9fca0
Vijay Manral <vijaymanral@hotmail.com>
2023-05-28 18:04:50 +0530

    Added tourists client

---
+++ client.go
@@ -0,0 +1,72 @@
+package tourists
+
+import (
+	"fmt"
+	"io/ioutil"
+	"net/http"
+	"time"
+)
+
+// HostURL - Default Tourists URL
+const HostURL string = "http://restapi.adequateshop.com"
+
+// Client -
+type Client struct {
+	HostURL    string
+	HTTPClient *http.Client
+}
+
+// AuthStruct -
+type AuthStruct struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
+}
+
+// AuthResponse -
+type AuthResponse struct {
+	UserID   int    `json:"user_id`
+	Username string `json:"username`
+	Token    string `json:"token"`
+}
+
+// NewClient -
+func NewClient(host *string) (*Client, error) {
+	c := Client{
+		HTTPClient: &http.Client{Timeout: 10 * time.Second},
+		// Default Tourists URL
+		HostURL: HostURL,
+	}
+
+	if host != nil {
+		c.HostURL = *host
+	}
+
+	return &c, nil
+}
+
+func (c *Client) doRequest(req *http.Request, authToken *string) ([]byte, error) {
+	token := c.Token
+
+	if authToken != nil {
+		token = *authToken
+	}
+
+	req.Header.Set("Authorization", token)
+
+	res, err := c.HTTPClient.Do(req)
+	if err != nil {
+		return nil, err
+	}
+	defer res.Body.Close()
+
+	body, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return nil, err
+	}
+
+	if res.StatusCode != http.StatusOK {
+		return nil, fmt.Errorf("status: %d, body: %s", res.StatusCode, body)
+	}
+
+	return body, err
+}
